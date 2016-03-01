53fbf5cfcaceec14b8595560764408b0f74dfc3e
Jennifer Han <jhan2@andrew.cmu.edu>
2016-02-29 21:06:43 -0500

    set up the validations and relationships for the Indicator model

--- app/models/indicator.rb
+++ app/models/indicator.rb
@@ -1,2 +1,26 @@
 class Indicator < ActiveRecord::Base
+
+	# Relationships
+	# --------------------------------
+	belongs_to :competency
+	has_many :indicator_resources
+	has_many :indicators, through: :indicator_resources
+
+	# Validations
+	# --------------------------------
+	validates_presence_of :competency_id, :level, :description
+	validates_numericality_of :competency_id, only_integer: true
+
+
+	# Scopes
+	# --------------------------------
+	scope :alphabetical, -> { order('description') }
+	scope :
+
+
+	# Methods
+	# --------------------------------
+
+
+
 end
