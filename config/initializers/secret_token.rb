2ea292bba9cf3da1ffb6cc734c38c9978b9b588f
James Ormond <jormond@andrew.cmu.edu>
2016-02-29 20:08:55 -0500

    Generated initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PDSAssessment::Application.config.secret_key_base = '7b3d78e0c5d84c49f577487bc7e3d7651b0aac2ed4da695f30f72f65b1eea3976accfd77c33805a4a84d148c30b097ff53b98753f4fcdad51c44b2f6c243d5e7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PDSAssessment::Application.config.session_store :cookie_store, key: '_PDS_Assessment_session'
