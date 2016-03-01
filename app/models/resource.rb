3dde802d6218dcd5f94b3af1bf17da09fb50c796
mogh <mogh@example.com>
2016-02-29 18:13:35 -0800

    added basic relations and validations for resources

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,2 +1,12 @@
 class Resource < ActiveRecord::Base
+	has_many :indicator_resources
+	has_many :indicator, through: :indicator_resources
+
+	scope :by_competency, -> { joins(:competency).order('competency.name') }
+
+    # Validations
+    # -----------------------------
+    validates_presence_of :type, :description, :link
+    validates_format_of :link, with: /(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/, message: "is not a valid format"
+
 end
