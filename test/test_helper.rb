9623d107279a19d63d5e211ea9e5c9cfbac8295d
Jennifer Han <jhan2@andrew.cmu.edu>
2016-03-01 09:37:51 -0500

    setting up simple_cov gem and turn gem in the test_helper.rb file

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'simplecov'
+SimpleCov.start 'rails'
+require 'turn/autorun'
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
@@ -9,7 +12,8 @@ class ActiveSupport::TestCase
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
-  fixtures :all
+  # fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  Turn.config.format = :outline
 end
