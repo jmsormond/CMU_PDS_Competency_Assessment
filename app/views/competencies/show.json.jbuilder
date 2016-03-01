8ba57251f6dcd391df0b2565236483fefd863831
James Ormond <jormond@andrew.cmu.edu>
2016-02-29 20:16:44 -0500

    Generated Competency model

---
+++ app/views/competencies/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @competency, :id, :name, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 PDSAssessment::Application.routes.draw do
+  resources :competencies
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
