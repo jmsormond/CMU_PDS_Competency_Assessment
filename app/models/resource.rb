05eaf80d0cab7d61a675b596676e03450b9213d4
mogh <mogh@example.com>
2016-03-02 17:46:57 -0800

    started tests for resources

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -6,11 +6,10 @@ class Resource < ActiveRecord::Base
     # -----------------------------
 	scope :alphabetical, -> { order('name') }
 	scope :active, -> { where(active: true) }
-	scope :by_indicator, -> { joins(:indicator_resource.order('indicator_resource.indicator_id') }
 
     # Validations
     # -----------------------------
-    validates_presence_of :type, :description
+    validates_presence_of :name, :type
     validates_format_of :link, with: /(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/, message: "is not a valid format"
 
 end
