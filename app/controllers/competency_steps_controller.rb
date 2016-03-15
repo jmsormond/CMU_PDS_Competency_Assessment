74f78882017b56d2390bb8120e14e0a9e514b654
mogh <mogh@example.com>
2016-03-14 19:30:42 -0700

    trying to fix passing on params

--- app/controllers/competency_steps_controller.rb
+++ app/controllers/competency_steps_controller.rb
@@ -3,8 +3,17 @@ class CompetencyStepsController < ApplicationController
 	steps :indicators, :resources, :verify
 
 	def show
-		@competency = Competency.find(params[:competency_id])
+		$competency = Competency.find(params[:competency_id])
+		@competency = $competency
     	render_wizard
+  	end
+
+  	def update
+  		puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  		puts params
+  		competency = Competency.find($competency.id)
+  		competency.attributes = competency.params
+  		render_wizard @competency
   	end
 
 end
