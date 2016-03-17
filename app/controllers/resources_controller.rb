4587c9ee28e313d2454191c5498ae4601f55b193
James Ormond <jormond@andrew.cmu.edu>
2016-03-16 22:58:25 -0400

    reduced pagination and changed header

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -15,7 +15,7 @@ class ResourcesController < ApplicationController
       }
     ) or return
 
-    @resources = Resource.filterrific_find(@filterrific).paginate(page: params[:page], per_page: 20)
+    @resources = Resource.filterrific_find(@filterrific).paginate(page: params[:page], per_page: 10)
 
     respond_to do |format|
       format.html
