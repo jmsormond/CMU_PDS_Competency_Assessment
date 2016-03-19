4400e4990f43f69db87a696dd5b5ecfc96ff20f1
James Ormond <jormond@andrew.cmu.edu>
2016-03-19 16:23:25 -0400

    installed gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
+    require 'smarter_csv'
 
     def home
-        
+        indicators = SmarterCSV.process('/tmp/Indicators.xlsx')
+        puts indicators
     end
 
 end
