102667963c16413973373467f342baee011c5132
James Ormond <jormond@andrew.cmu.edu>
2016-03-19 21:56:18 -0400

    using CSV, able to import csv files

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
 
     def home
-        indicators = SmarterCSV.process('./public/Indicators.csv')
-        puts indicators
+        indicators = CSV.read('public/Indicators.csv', {:headers => true, :encoding => 'windows-1251:utf-8'})
+        indicators.each do |indicator|
+            puts "-----------"
+            puts indicator
+        end
     end
 
 end
