7735e5020243aacfaab83e3e08920699c1f6e21a
mogh <mogh@example.com>
2016-03-20 13:09:39 -0700

    fixed up finite state machine

--- app/controllers/competency_steps_controller.rb
+++ app/controllers/competency_steps_controller.rb
@@ -2,15 +2,15 @@ class CompetencyStepsController < ApplicationController
 	include Wicked::Wizard
   include AssessmentHelpers::Builder
   
-  steps :name, :indicators, :resources, :verify
+  steps :indicators, :resources, :verify
 
 	def show
     case step
-    when :name
-      @competency = competency.new
-      session[:competency] = nil
+    when :indicators
+      @competency = Competency.find(params[:competency_id])
+      session[:Competency] = @competency.attributes
     else
-      @competency = competency.new(session[:competency])
+      @competency = Competency.new(session[:competency])
     end
     render_wizard
   end
@@ -20,20 +20,18 @@ class CompetencyStepsController < ApplicationController
   	#@competency.update_attributes(params[:competency])
   	#render_wizard @competency
     case step
-    when :name
-      @competency = Competency.new(competency_params)
-      session[:Competency] = @competency.attributes
-      redirect_to next_wizard_path
     when :indicators
       session[:competency] = session[:competency].merge(params[:competency])
       @competency = Competency.new(session[:competency])
       @competency.save
-      redirect_to competency_path(@competency)
+      redirect_to next_wizard_path
     when :resources
       session[:competency] = session[:competency].merge(params[:competency])
       @competency = Competency.new(session[:competency])
+      redirect_to next_wizard_path
     when :verify
       @competency.save
+      redirect_to competency_path(@competency)
     end
   end
 
