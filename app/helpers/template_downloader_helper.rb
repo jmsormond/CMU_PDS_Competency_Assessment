a2b662e0795c33bd4e85e6a74110d46c90976cbe
James Ormond <jormond@andrew.cmu.edu>
2016-03-20 20:30:47 -0400

    template downloading now works

---
+++ app/helpers/template_downloader_helper.rb
@@ -0,0 +1,2 @@
+module TemplateDownloaderHelper
+end
---
+++ test/controllers/template_downloader_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TemplateDownloaderControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
