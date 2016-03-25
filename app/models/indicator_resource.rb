626ee3e3fa195871211ea8da16c363bc444be7ef
mogh <mogh@example.com>
2016-03-25 16:07:51 -0700

    wizard basic functionality complete

--- app/models/indicator_resource.rb
+++ app/models/indicator_resource.rb
@@ -3,4 +3,8 @@ class IndicatorResource < ActiveRecord::Base
 	belongs_to :resource
 	belongs_to :indicator
 	validates_presence_of :resource_id, :indicator_id
+
+	def name
+		return resource.name
+	end
 end
