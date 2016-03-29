fc718be1adb01739eb8e490c775917bae3c375b1
James Ormond <jormond@andrew.cmu.edu>
2016-03-29 12:42:13 -0400

    100% coverage for question

--- test/models/indicator_question_test.rb
+++ test/models/indicator_question_test.rb
@@ -4,4 +4,12 @@ class IndicatorQuestionTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  # Relationship Macros
+  should belong_to(:questions)
+  should belong_to(:indicators)
+
+  # Validation Macros
+  should validate_presence_of(:indicator_id)
+  should validate_presence_of(:question_id)
 end
