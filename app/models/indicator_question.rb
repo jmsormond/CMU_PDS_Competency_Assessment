b768e1ba532c2dba5ca77e7cc543ee2eb0d06d67
James Ormond <jormond@andrew.cmu.edu>
2016-03-29 11:51:14 -0400

    set up model for indicator_question

--- app/models/indicator_question.rb
+++ app/models/indicator_question.rb
@@ -1,2 +1,8 @@
 class IndicatorQuestion < ActiveRecord::Base
+    #relationships
+    belongs_to :question
+    belongs_to :indicator
+
+    #validations
+    validates_presence_of :indicator_id, :resource_id
 end
