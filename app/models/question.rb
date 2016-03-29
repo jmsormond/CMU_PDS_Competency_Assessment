edf4d1e2564ba084a06842c96e3999076b2270c7
James Ormond <jormond@andrew.cmu.edu>
2016-03-29 11:50:00 -0400

    set up model for question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,13 @@
 class Question < ActiveRecord::Base
+    #relationships
+    has_many :indicator_questions
+    has_many :indicators, through: :inidcator_questions
+
+    #validations
+    validates_presence_of :question
+
+    #scoeps
+    scope :active, -> { where("active = ?", true) }
+    scope :inactive, -> { where("active = ?", false) }
+    scope :alphabetical, -> { order("name") }
 end
