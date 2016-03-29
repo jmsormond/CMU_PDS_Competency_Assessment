06f8f1b3cf75c6de6ed191f6dee4e3f1aa71b4ee
James Ormond <jormond@andrew.cmu.edu>
2016-03-29 08:45:14 -0400

    got CSV reader to work

--- app/controllers/competency_steps_controller.rb
+++ app/controllers/competency_steps_controller.rb
@@ -36,7 +36,34 @@ class CompetencyStepsController < ApplicationController
   end
 
   def upload
-    puts "SDJKGLHSDGKJHSDKJGHSKJHGKSJDHGKJSDHFKSLH"
+    indicator_info = Array.new
+    indicator_file = params[:indicators]
+    file = CSV.new(indicator_file.path)
+    # CSV.foreach(indicator_file.path) do |row|
+    #   id, level, name = row
+    #   indicator_info.push({id: id, level: level, name: name})
+    # end
+    indicators = CSV.read(indicator_file.path, {:headers => true, :encoding => 'windows-1251:utf-8'})
+    indicators.each do |indicator|
+        indicator_info.push({id: indicator[0], level: indicator[1], name: indicator[2]})
+    end
+    puts indicator_info
+    # update_competency_indicators(indicator_info)
+
+    # resource_info = Array.new
+    # resources = CSV.read('public/Resources.csv', {:headers => true, :encoding => 'windows-1251:utf-8'})
+    # resources.each do |resource|
+    #     resource_info.push({id: resource[0], resource_category: resource[1], name: resource[2], description: resource[3], link: resource[4]})
+    # end
+    # puts resource_info
+    # # update_competency_resources(resource_info)
+
+    # relationship_info = Array.new
+    # relationships = CSV.read('public/Relationships.csv', {:headers => true, :encoding => 'windows-1251:utf-8'})
+    # relationships.each do |relationship|
+    #     relationship_info.push({indicator_id: relationship[0], resource_id: relationship[1]})
+    # end
+    # puts relationship_info
   end
 
   def competency_params
