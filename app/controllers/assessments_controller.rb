5d4c0f152171e2d8343c3cf9b107530eab007d56
James Ormond <jormond@andrew.cmu.edu>
2016-04-11 22:42:38 -0400

    changed form format using answer attr_accessor for questions

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -18,8 +18,46 @@ class AssessmentsController < ApplicationController
         end
     end
 
-    def report
+    def generate_report
+        puts params
+        # @competency = Competency.find(params[:competency])
+        # @questions = Question.all.by_competency(@competency.id)
+        # size = @questions.size
+        # position = 1
+        # answers = Hash.new
+        # while position <= size
+        #     # answer = get_answer(params, position)
+        #     # @questions[position - 1].answer = answer
+        #     @questions[position - 1].answer = "test answer goes here !!!!!"
+        #     puts @questions[position - 1].attributes
+        #     position = position + 1
+        # end
+
+        render 'present_report'
+
+    end
+
+    def present_report
         
+    end
+
+    private
+    def get_answer(parameters, position)
+        if parameters["always" << position]
+            return "always"
+        elsif parameters["often" << position]
+            return "often"
+        elsif parameters["sometimes" << position]
+            return "sometimes"
+        elsif parameters["rarely" << position]
+            return "rarely"
+        elsif parameters["never" << position]
+            return "never"
+        elsif parameters["does_not_apply" << position]
+            return "does_not_apply"
+        else
+            return "no_answer"
+        end
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,8 @@ PDSAssessment::Application.routes.draw do
   get 'assessments/welcome' => 'assessments#welcome', as: :assessment_welcome
   get 'assessments/choose' => 'assessments#choose', as: :assessment_choose
   get 'assessments/take_assessment' => 'assessments#take_assessment', as: :take_assessment
-  get 'assessments/report' => 'assessments#report', as: :assessment_report
+  post 'assessments/report' => 'assessments#generate_report', as: :generate_assessment_report
+  get 'assessments/report' => 'assessments#present_report', as: :assessment_report
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
