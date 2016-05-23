542b99517dcae02fa5743f907dfc3ec56532812d
James Ormond <jormond@andrew.cmu.edu>
2016-05-23 00:14:48 -0400

    updated training manual

--- app/controllers/template_downloader_controller.rb
+++ app/controllers/template_downloader_controller.rb
@@ -13,7 +13,7 @@ class TemplateDownloaderController < ApplicationController
     end
 
     def training_manual
-        filename = "Training Manual.pdf"
+        filename = "Training_Manual.pdf"
         send_file(
             "#{Rails.root}/public/" << filename,
             filename: filename
